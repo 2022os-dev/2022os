f5ed416a85d64b2e8f15a9200a5b7f83552fd303
panpan <panpansuannai@outlook.com>
2022-03-27 12:45:23 +0800

    Add: fix boot stack

--- src/entry.rs
+++ src/entry.rs
@@ -7,7 +7,10 @@ extern "C" fn _start() {
     unsafe {
         asm!(
         "mv tp, a0",
-        "la sp, boot_stack",
+        "la sp, boot_stack_top",
+        "li a1, 4096",
+        "mul a0, a0, a1",
+        "sub sp, sp, a0",
         "j kernel_start", options(noreturn));
     }
 }
@@ -15,7 +18,7 @@ global_asm!(
     ".section .bss.stack
     .globl boot_stack
 boot_stack:
-    .space 32
+    .space 4096 * 4
     .globl boot_stack_top
 boot_stack_top:"
 );
