e8af6f28f4572c64b812474f56c2e6dcc86f4f36
panpan <panpansuannai@outlook.com>
2022-03-28 08:03:17 +0800

    Fix: don't use naked entry

--- src/entry.rs
+++ src/entry.rs
@@ -1,21 +1,20 @@
-use crate::asm;
 use core::arch::global_asm;
-#[naked]
-#[no_mangle]
-#[link_section = ".text.entry"]
-extern "C" fn _start() {
-    unsafe {
-        asm!(
-        "mv tp, a0",
-        "la sp, boot_stack_top",
-        "li a1, 16384",
-        "mul a0, a0, a1",
-        "sub sp, sp, a0",
-        "j kernel_start", options(noreturn));
-    }
-}
+
 global_asm!(
-    ".section .bss.stack
+    ".section .text.entry
+    .globl _start
+_start:
+    addi a0, a0, 65
+    li a7, 1
+    ecall
+    mv tp, a0
+    la sp, boot_stack_top
+    li a1, 16384
+    mul a0, a0, a1
+    sub sp, sp, a0
+    j kernel_start
+
+    .section .bss.stack
     .globl boot_stack
 boot_stack:
     .space 16384 * 4
