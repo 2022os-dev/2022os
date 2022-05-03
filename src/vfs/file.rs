e604ed2e8490178d442d14f20a9169299868b03f
panpan <panpansuannai@outlook.com>
2022-05-03 13:32:12 +0800

    Add: no user output

--- src/vfs/file.rs
+++ src/vfs/file.rs
@@ -328,6 +328,7 @@ impl _Inode for Console {
     }
     fn write_offset(&self, _: usize, buf: &[u8]) -> Result<usize, FileErr> {
         unsafe {
+            #[cfg(not(feature = "no_user_output"))]
             print!("{}", core::str::from_utf8_unchecked(buf));
         }
         Ok(buf.len())
